9a741f9fd8415dc42ae5b83a1b837b0c96a79a39
Aakef Waris <awaris@hawk.iit.edu>
2019-10-19 15:10:25 -0500

    added test script for sms

---
+++ twilio/app.py
@@ -0,0 +1,18 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'AC4a171ebb76aac736dce2958d1ffeabd4'
+auth_token = '8d3af93e4c6ccffc94360c3fd7cc5566'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+13368021545',
+                     to='+16309911662'
+                 )
+
+print(message.sid)
