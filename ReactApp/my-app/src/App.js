9a7716af7437fdd0a4505bc64bbc559531465cad
Zhimin90 <zhiminzou@gmail.com>
2019-10-19 15:53:45 -0500

    Changed react package

--- ReactApp/my-app/src/App.js
+++ ReactApp/my-app/src/App.js
@@ -1,33 +1,55 @@
-import React, { Component } from "react";
-import GoogleMapReact from "google-map-react";
-import logo from "./logo.svg";
-import "./App.css";
+/*global google*/
+import React from "react";
+import ReactDOM from "react-dom";
+//import { DirectionsRenderer } from "react-google-maps";
+import { compose, withProps, lifecycle } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  DirectionsRenderer
+} from "react-google-maps";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+//const google=window.google
+//import { DirectionsRenderer } from "react-google-maps";
 
-class App extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
+const MapWithADirectionsRenderer = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAcQjrfAudzl6Ton7GA7D-gVqOINMFE7ns&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap,
 
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyDgFVINsOlKy7krD7p0eleGgHkzULtPEr8" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-}
+  lifecycle({
+    componentDidMount() {
+      const DirectionsService = new google.maps.DirectionsService();
 
-export default App;
+      DirectionsService.route(
+        {
+          origin: new google.maps.LatLng(40.712776, -74.005974),
+          destination: new google.maps.LatLng(34.052235, -118.243683),
+          travelMode: google.maps.TravelMode.DRIVING
+        },
+        (result, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            this.setState({
+              directions: result
+            });
+          } else {
+            console.error(`error fetching directions ${result}`);
+          }
+        }
+      );
+    }
+  })
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    {props.directions && <DirectionsRenderer directions={props.directions} />}
+  </GoogleMap>
+));
+
+export default MapWithADirectionsRenderer;
